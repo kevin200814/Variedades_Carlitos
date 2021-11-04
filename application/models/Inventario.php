b9dc8c7b048962686cc79b1cd1cd4cbcbcebd700
diegoalvaradousam <rhamphorychus.sthich@gmail.com>
2021-11-04 17:04:41 -0600

    a2

--- application/models/Inventario.php
+++ application/models/Inventario.php
@@ -5,11 +5,11 @@ Class Inventario extends CI_Model{
 
     public function obtener_inventario(){
 
-        $this->db->join('TBL_CATEGORIA C','C.ID_CATEGORIA = P.ID_CATEGORIA');
-        $this->db->join('TBL_GENERO G','C.ID_GENERO = G.ID_GENERO');
-        $this->db->join('TBL_TALLA T','C.ID_TALLA = T.ID_TALLA');
-        $this->db->join('TBL_COLORES CO','C.ID_COLORES = CO.ID_COLORES');
-        $this->db->join('TBL_MARCA M','C.ID_TALLA = M.ID_MARCA');
+        $this->db->join('TBL_CATEGORIA C','P.ID_CATEGORIA = P.ID_CATEGORIA');
+        $this->db->join('TBL_GENERO G','P.ID_GENERO = G.ID_GENERO');
+        $this->db->join('TBL_TALLA T','P.ID_TALLA = T.ID_TALLA');
+        $this->db->join('TBL_COLORES CO','P.ID_COLORES = CO.ID_COLORES');
+        $this->db->join('TBL_MARCA M','P.ID_TALLA = M.ID_MARCA');
         $query = $this->db->get('TBL_PRODUCTOS P');
         return $query->result();
     }
